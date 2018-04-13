415147166fa137d4f76021dd39d07c5f96916b42
Reka <bartha1reka@gmail.com>
2018-04-13 22:37:53 +0300

    reverse transform

--- src/MoveToFront.cpp
+++ src/MoveToFront.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include "MoveToFront.h"
 
-MoveToFront::MoveToFront(const string & input, const string & alphabet) : input(input), alphabet(alphabet), output(input.length())
+MoveToFront::MoveToFront(const string & alphabet) : initial_alphabet(alphabet)
 {
 }
 
@@ -25,14 +25,31 @@ void MoveToFront::MoveCharToFront(unsigned int index)
     rotate(first, middle, last);
 }
 
-vector<unsigned int> MoveToFront::GetTransformedInput()
+vector<unsigned int> MoveToFront::encode(string input)
 {
-    auto length = output.size();
+    alphabet = initial_alphabet;
+    auto length = input.size();
+    vector<unsigned int> output(length);
 
     for (auto i = 0; i < length; i++)
     {
         auto index = IndexOfChar(input[i]);
         output[i] = index;
+        MoveCharToFront(index);
+    }
+
+    return output;
+}
+string MoveToFront::decode(vector<unsigned int> input)
+{
+    alphabet = initial_alphabet;
+    auto length = input.size();
+    string output(length, '0');
+
+    for (auto i = 0; i < length; i++)
+    {
+        auto index = input[i];
+        output[i] = alphabet[index];
         MoveCharToFront(index);
     }
 
--- src/MoveToFront.h
+++ src/MoveToFront.h
@@ -7,14 +7,14 @@ using namespace std;
 
 class MoveToFront {
 private:
-    string input;
-    vector<unsigned int> output;
+    string initial_alphabet;
     string alphabet;
 
     unsigned int IndexOfChar(char c);
     void MoveCharToFront(unsigned int index);
-    
+
 public:
-    MoveToFront(const string& input, const string& alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789,.?!;:");
-    vector<unsigned int> GetTransformedInput();
+    MoveToFront(const string& alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789,.?!;:");
+    vector<unsigned int> encode(string input);
+    string decode(vector<unsigned int> input);
 };
